7049c3da21b750a4cbb22f6f528b2ff8b6dcf5ee
CarmeloMelito <melitocarmelo211@gmail.com>
2025-02-25 19:51:35 +0100

    add:multiples 5

--- main.js
+++ main.js
@@ -2,8 +2,10 @@
 for (let n = 1; n < 101; n++) {
     if (n % 3 == 0)
         console.log("Fizz");
-    console.log(n)
-}
+    else if (n % 5 == 0)
+        console.log("Buzz");
 
+    console.log(n);
+}
 
 //console.log(n)
