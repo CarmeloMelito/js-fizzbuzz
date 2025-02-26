d5ed88728ee66a4882992888e2077102a64101d0
CarmeloMelito <melitocarmelo211@gmail.com>
2025-02-26 09:37:28 +0100

    feat:fix multiples and finish

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 //Creazione Ciclo For
 for (let n = 1; n < 101; n++) {
-    if (n % 3 == 0)
+    //CONDIZIONI
+
+    //FIZZBUZZ
+
+    if (n % 15 == 0)
+        console.log("FizzBuzzz");
+    //FIZZ
+
+    else if (n % 3 == 0)
         console.log("Fizz");
+    //BUZZ
     else if (n % 5 == 0)
         console.log("Buzz");
 
-    console.log(n);
+    else console.log(n);
 }
 
 //console.log(n)
